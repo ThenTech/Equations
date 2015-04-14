0abeed1eb19a7a049bbb3ca10e51111b94995f70
Wosser1sProductions <Wosser1sProductions@users.noreply.github.com>
2015-04-14 23:08:03 +0200

    Update _equationTester.c
    + Uses equations.h or rpoly.h

--- _equationTester.c
+++ _equationTester.c
@@ -1,4 +1,13 @@
+/*
+*	Toggle the comment on the header you want to use.
+*		equations.h		Standard functions regarding equations;
+*						e.g. reading, printing, basic solver and operations.
+*
+*		rpoly.h			Advanced eaquation solver with read/print functions. 
+*						Finds all solutions in both real and complex planes.
+*/
 #include "equations.h"
+//#include "rpoly.h"
 
 /**
 *	Parse the input and give the equation output.
@@ -11,12 +20,17 @@
 *	No args? Read from user input.
 */
 int main(int argc, char *argv[]) {
-	double *equation;
 	int readFromCmd = argc - 2;
+	EQUATION e;
 
-	while (makeEquation(&equation, argv, readFromCmd) > 1) { readFromCmd = 0; putchar('\n'); };
+#ifdef _USE_RPOLY
+	while (makeEquation_ak1(&e, argv, readFromCmd) > 0)
+#else
+	while (makeEquation(&e, argv, readFromCmd) > 0)		
+#endif
+		{ readFromCmd = 0; putchar('\n'); };
 
 	printf("\n");
-	//system("PAUSE");
+	system("PAUSE");
 	return 0;
 }
